56344e10e5b89b2adba11abf71c8a82de9cbf4e7
rahafalnassrallah <rahafalnasralah112233@gmail.com>
2023-04-13 00:46:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mylab4/StudentMod.java
@@ -0,0 +1,46 @@
+package com.example.mylab4;
+
+public class StudentMod {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public StudentMod(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentMod{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
